c1f6e24849f2ae63060add28ca5256f44ebade2d
Coffee58511867 <marobahlalele910@gmail.com>
2023-06-10 09:45:18 +0200

    Created tab pages

--- lib/profile.dart
+++ lib/profile.dart
@@ -17,6 +17,58 @@ class _ProfilePageState extends State<ProfilePage> {
         automaticallyImplyLeading: false,
         backgroundColor: Colors.blue,
       ),
+      body: const Profile(),
+    );
+  }
+}
+
+class Profile extends StatelessWidget {
+  const Profile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(32.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              "User Profile",
+              style: TextStyle(
+                  fontFamily: AutofillHints.addressState, fontSize: 25.0),
+            ),
+            const SizedBox(height: 16.0),
+            const Text(
+              "Email Address",
+              style: TextStyle(
+                  fontFamily: AutofillHints.addressState, fontSize: 25.0),
+            ),
+            SizedBox(height: 16.0),
+            Text(
+              "User Address",
+              style: TextStyle(
+                  fontFamily: AutofillHints.addressState, fontSize: 25.0),
+            ),
+            SizedBox(height: 16.0),
+            Row(
+              children: [
+                Text(
+                  "User Profile",
+                  style: TextStyle(
+                      fontFamily: AutofillHints.addressState, fontSize: 25.0),
+                ),
+                Text(
+                  "hlalelemaroa@gmail.com",
+                  style: TextStyle(
+                      fontFamily: AutofillHints.addressState, fontSize: 18.0),
+                ),
+                Container()
+              ],
+            )
+          ],
+        ),
+      ),
     );
   }
 }

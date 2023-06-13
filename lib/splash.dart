58b1a4e4cdfaada6425a0e513393efc3c8ada6cd
Coffee58511867 <marobahlalele910@gmail.com>
2023-06-13 13:20:12 +0200

    Added Splash Screen

---
+++ lib/splash.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+
+class SplashPage extends StatefulWidget {
+  const SplashPage({super.key});
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Notifications Page"),
+        centerTitle: true,
+        backgroundColor: Colors.blue,
+      ),
+      body: const SplashScreen(),
+    );
+  }
+}
+
+class Splash extends StatefulWidget {
+  const Splash({super.key});
+
+  @override
+  State<Splash> createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+  @override
+  void initState() {
+    super.initState();
+    _navigateToHome();
+  }
+
+  Future<void> _navigateToHome() async {
+    await Future.delayed(const Duration(seconds: 2));
+    Navigator.pushReplacementNamed(context, '/home');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(100.0),
+        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          Image.network(
+            'https://s3-us-west-2.amazonaws.com/cbi-image-service-prd/modified/f3e92246-1ef8-4b6e-95aa-676b8d48e3c6.png',
+            width: 300,
+            height: 200,
+          ),
+          const SizedBox(height: 16),
+          const Text(
+            "You are welcome",
+            style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(height: 16),
+          const CircularProgressIndicator(),
+        ]),
+      ),
+    );
+  }
+}
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    _navigateToHome();
+  }
+
+  Future<void> _navigateToHome() async {
+    await Future.delayed(const Duration(seconds: 2));
+    Navigator.pushReplacementNamed(context, '/home');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: Colors.blue,
+        child: const Center(
+          child: Text(
+            "Welcome",
+            style: TextStyle(
+                fontSize: 34.0,
+                fontWeight: FontWeight.w700,
+                color: Colors.white),
+          ),
+        ),
+      ),
+    );
+  }
+}

0628e66189d7fcf95aa14eedfcf6ae297a827543
Coffee58511867 <marobahlalele910@gmail.com>
2023-06-13 13:28:58 +0200

    Added Splash Screen

--- lib/splash.dart
+++ lib/splash.dart
@@ -79,7 +79,7 @@ class _SplashScreenState extends State<SplashScreen> {
   }
 
   Future<void> _navigateToHome() async {
-    await Future.delayed(const Duration(seconds: 2));
+    await Future.delayed(const Duration(seconds: 3));
     Navigator.pushReplacementNamed(context, '/login');
   }
 

01f3f429c30d21c1dcd9787031bbe3349890fccf
Coffee58511867 <marobahlalele910@gmail.com>
2023-06-13 11:33:33 +0200

    Created new app

--- lib/ecocash.dart
+++ lib/ecocash.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 class EcocashPage extends StatefulWidget {
   const EcocashPage({super.key});
@@ -70,18 +71,38 @@ class _MpesaState extends State<Mpesa> {
       };
       try {
         // Send the data to Firestore
-        await FirebaseFirestore.instance.collection('users').add(userData);
+        await FirebaseFirestore.instance.collection('payments').add(userData);
 
         // Clear fields
         //clear fields
         amountController.clear();
         phoneController.clear();
+        Fluttertoast.showToast(
+          msg: "Payment Made Successfully",
+          toastLength: Toast
+              .LENGTH_SHORT, // Duration for which the toast will be visible
+          gravity: ToastGravity
+              .CENTER, // Position of the toast message on the screen
+          backgroundColor:
+              Colors.black54, // Background color of the toast message
+          textColor: Colors.green, // Text color of the toast message
+        );
 
         // Navigate to dashboard
         Navigator.pushNamed(context, '/');
       } catch (e) {
         // Handle any errors that occur during the data submission
         print('Error submitting data: $e');
+        Fluttertoast.showToast(
+          msg: "Something went wrong please try again",
+          toastLength: Toast
+              .LENGTH_SHORT, // Duration for which the toast will be visible
+          gravity: ToastGravity
+              .CENTER, // Position of the toast message on the screen
+          backgroundColor:
+              Colors.black54, // Background color of the toast message
+          textColor: Colors.red, // Text color of the toast message
+        );
       }
 
       //navigate to dashboard
